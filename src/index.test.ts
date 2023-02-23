eaac206d62833bb77d7f59a0cd783301bfc11724
Alex González <alejandrogsa@gmail.com>
2023-02-23 10:21:18 +0100

    remove unnecesary index

--- src/index.test.ts
+++
@@ -1,7 +0,0 @@
-import { hello } from './index';
-
-describe('Config example verification', () => {
-  test('Hello must have content', () => {
-    expect(hello.hello).toBe('Hello world');
-  });
-});
