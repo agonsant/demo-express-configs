60c472cb29184c631031be59374bf946b8d711ea
Alex González <alejandrogsa@gmail.com>
2023-02-28 17:43:15 +0100

    remove console log

--- src/logger.ts
+++ src/logger.ts
@@ -3,7 +3,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const log = bunyan.createLogger({ name: 'DemoApp' });
-console.log(process.env.BUNYAN_LEVEL);
 log.level((process.env.BUNYAN_LEVEL as LogLevel) ?? 'info');
 
 export default log;
